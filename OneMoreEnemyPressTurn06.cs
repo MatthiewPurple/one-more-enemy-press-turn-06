e1b075f082e0aa8f43657a74d3990454bf6c2afd
Matthiew Purple <matthiewpurple@hotmail.com>
2023-04-07 23:22:23 +0200

    Add project files.

---
+++ OneMoreEnemyPressTurn06.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using Il2Cpp;
+using one_more_enemy_press_turn_06;
+
+[assembly: MelonInfo(typeof(OneMoreEnemyPressTurn06), "One more enemy Press Turn (ver. 0.6)", "1.0.0", "Matthiew Purple")]
+[assembly: MelonGame("アトラス", "smt3hd")]
+
+namespace one_more_enemy_press_turn_06;
+public class OneMoreEnemyPressTurn06 : MelonMod
+{
+    [HarmonyPatch(typeof(nbMainProcess), nameof(nbMainProcess.nbSetPressMaePhase))]
+    private class Patch
+    {
+        public static void Postfix()
+        {
+            short activeunit = nbMainProcess.nbGetMainProcessData().activeunit;
+            if (activeunit >= 4 && nbMainProcess.nbGetMainProcessData().press4_p < 8)
+            {
+                nbMainProcess.nbGetMainProcessData().press4_p++;
+                nbMainProcess.nbGetMainProcessData().press4_ten++;
+            }
+        }
+    }
+}
