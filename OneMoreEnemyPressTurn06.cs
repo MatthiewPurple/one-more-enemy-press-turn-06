3ddf0436cc03e097742f148b8c9c9685d54a4c95
Matthiew Purple <matthiewpurple@hotmail.com>
2023-04-07 23:30:38 +0200

    comments!!!

--- OneMoreEnemyPressTurn06.cs
+++ OneMoreEnemyPressTurn06.cs
@@ -9,16 +9,19 @@ using one_more_enemy_press_turn_06;
 namespace one_more_enemy_press_turn_06;
 public class OneMoreEnemyPressTurn06 : MelonMod
 {
+    // After initiating a phase
     [HarmonyPatch(typeof(nbMainProcess), nameof(nbMainProcess.nbSetPressMaePhase))]
     private class Patch
     {
         public static void Postfix()
         {
-            short activeunit = nbMainProcess.nbGetMainProcessData().activeunit;
+            short activeunit = nbMainProcess.nbGetMainProcessData().activeunit; // Get the formindex of the first active demon
+
+            // If that demon is an enemy and the enemies don't already have 8 press turns
             if (activeunit >= 4 && nbMainProcess.nbGetMainProcessData().press4_p < 8)
             {
-                nbMainProcess.nbGetMainProcessData().press4_p++;
-                nbMainProcess.nbGetMainProcessData().press4_ten++;
+                nbMainProcess.nbGetMainProcessData().press4_p++; // Add 1 full press turn
+                nbMainProcess.nbGetMainProcessData().press4_ten++; // Add 1 total press turn
             }
         }
     }
