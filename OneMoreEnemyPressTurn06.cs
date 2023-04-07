6120201e04c0cd39ec290e33bd840820a922b485
Matthiew Purple <matthiewpurple@hotmail.com>
2023-04-07 23:45:17 +0200

    bosses only

--- OneMoreEnemyPressTurn06.cs
+++ OneMoreEnemyPressTurn06.cs
@@ -2,8 +2,9 @@
 using HarmonyLib;
 using Il2Cpp;
 using one_more_enemy_press_turn_06;
+using Il2Cppnewdata_H;
 
-[assembly: MelonInfo(typeof(OneMoreEnemyPressTurn06), "One more enemy Press Turn (ver. 0.6)", "1.0.0", "Matthiew Purple")]
+[assembly: MelonInfo(typeof(OneMoreEnemyPressTurn06), "One more enemy Press Turn [Bosses only] (ver. 0.6)", "1.0.0", "Matthiew Purple")]
 [assembly: MelonGame("アトラス", "smt3hd")]
 
 namespace one_more_enemy_press_turn_06;
@@ -17,12 +18,26 @@ public class OneMoreEnemyPressTurn06 : MelonMod
         {
             short activeunit = nbMainProcess.nbGetMainProcessData().activeunit; // Get the formindex of the first active demon
 
-            // If that demon is an enemy and the enemies don't already have 8 press turns
-            if (activeunit >= 4 && nbMainProcess.nbGetMainProcessData().press4_p < 8)
+            // If that demon is an enemy, they have a boss and their side and the enemies don't already have 8 press turns
+            if (activeunit >= 4 && Utility.hasABossOnTheirSide() && nbMainProcess.nbGetMainProcessData().press4_p < 8)
             {
                 nbMainProcess.nbGetMainProcessData().press4_p++; // Add 1 full press turn
                 nbMainProcess.nbGetMainProcessData().press4_ten++; // Add 1 total press turn
             }
+        }
+    }
+
+    private class Utility
+    {
+        // Checks if there is a boss on the enemy's side
+        public static bool hasABossOnTheirSide()
+        {
+            foreach (datUnitWork_t item in nbMainProcess.nbGetMainProcessData().enemyunit)
+            {
+                if (item.id >= 256) return true; // All bosses have an ID of at least 256
+            }
+
+            return false;
         }
     }
 }
